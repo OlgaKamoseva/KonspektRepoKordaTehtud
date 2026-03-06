52ba83730cac2e95b776a00b473189dcec06a03b
OlgaKamoseva <olga.kamoseva@tthkee.onmicrosoft.com>
2026-03-06 15:53:48 +0200

    hesthtwe

---
+++ Naidisylesanded peale meetodeid/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Naidisylesanded_peale_meetodeid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("kasutajatanav.txt"))
+            {
+                string[] tervitus = File.ReadAllLines("kasutajatanav.txt");
+                Console.WriteLine(tervitus[0]);
+            }
+            else
+            {
+                Console.WriteLine("Sisesta oma nimi");
+                string kasutajanimi = GetAnswer();
+                Console.WriteLine("Tere, "+kasutajanimi);
+                Console.WriteLine("Palun sisesta ka oma tanavanimi");
+                string tanavanimi = GetAnswer();
+                string failisisu = kasutajanimi + " ahaa! Elad " + tanavanimi + " tanaval!";
+                Console.WriteLine(failisisu);
+                File.WriteAllText("kasutajatanav.txt", failisisu);
+            }
+        }
+
+        private static string GetAnswer()
+        {
+            Console.WriteLine("Sisesta siia:");
+            string vastus = Console.ReadLine();
+            return vastus;
+        }
+    }
+}
